56c8c4a205b1128ad5df1ecd7856971a70fcf6ad
vigneshxcode1 <vigneshxcode@gmail.com>
2026-02-15 07:58:02 +0530

    Fix: remove embedded repository

--- gittest/src/App.jsx
+++ gittest/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is update{count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
